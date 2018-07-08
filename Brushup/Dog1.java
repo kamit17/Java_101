8d804b527d7d38b7d6cb028e652b47bcab27eca2
kamit17 <kamit17@outlook.com>
2018-07-07 21:45:08 -0500

    Java Brushup

---
+++ Brushup/Dog1.java
@@ -0,0 +1,25 @@
+//Class Declaration
+class Dog1{
+	//Instance Variables
+	String breed;
+    String size;
+    int age;
+    String color;
+
+  
+    // method 1
+    public String getInfo() {
+        return ("Breed is: "+breed+" Size is:"+size+" Age is:"+age+" color is: "+color);
+    }
+}
+
+public class Execute{
+	public static void main(String[] args) {
+		Dog1 maltese = new Dog1();
+        maltese.breed="Maltese";
+        maltese.size="Small";
+        maltese.age=2;
+        maltese.color="white";
+        System.out.println(maltese.getInfo());
+	}
+}
---
+++ Brushup/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+	public static void main(String args[]){
+		SubClassB a = new SubClassB();
+		
+		a.foo();
+		a.bar();
+	}
+}
