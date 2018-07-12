bcd0bd96c4d6c52d901c529444efeaf050dc758d
kamit17 <kamit17@outlook.com>
2018-07-11 21:55:09 -0500

    Lynda

---
+++ Lynda/Ch03/03_04/IDEAProject/src/com/example/java/Main.java
@@ -0,0 +1,13 @@
+package com.example.java;
+
+import java.sql.SQLOutput;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello ");
+        String aString = "David";
+        System.out.println("Your name is "+ aString);
+    }
+}
--- Lynda/Ch04/04_02/PrimitiveNumbers/src/com/example/java/MaxValues.java
+++ Lynda/Ch04/04_02/PrimitiveNumbers/src/com/example/java/MaxValues.java
@@ -3,7 +3,31 @@ package com.example.java;
 public class MaxValues {
 
     public static void main(String[] args) {
+        byte b = 127;
+        short sh = 32767;
+        int i = 2147483647;
+        long l = (long) 9.2233789;
+
+        //float f = 1f;
+        //double d = 1d;
+        System.out.println("Byte: " + b);
+        System.out.println("Short: " + sh);
+        System.out.println("long: " + l);
+
+        if (b < Byte.MAX_VALUE) {
+            b++;
+        }
+        if ( sh < Short.MAX_VALUE)
+        {
+            sh++;
+        }
+        if ( l < Long.MAX_VALUE)
+        {
+            l++;
+        }
+        System.out.println("Byte: " + b);
+        System.out.println("Short: " + sh);
+        System.out.println("long: " + l);
 
     }
-
 }
